4c5e3ea259b14485360aa6167787d36e3c83269f
Kacper Ceszke <kazkwincjusz@outlook.com>
2025-03-23 16:07:12 +0100

    Second Commit

--- watchlist_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("list/", views.Movie_list, name="movie-list"),
-    path("<int:pk>", views.Movie_details, name="movie-details")
-]
